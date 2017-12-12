9b697aadd744eaf9047372b558e431e4e746045d
Rodrigo Tschope <tschope@gmail.com>
2017-12-12 10:16:20 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# IRP Class / GNIB
+
+A simple PHP API extension for retrive available dates from Ireland GNIB / IRP. I transform in [Carbon](http://carbon.nesbot.com) dates after retrive dates from Website.
+
+Very simple to use:
+
+```php
+$params = [];
+$params['cat'] = 'Work';
+$params['sbcat'] = 'All';
+$params['typ'] = 'Renewal';
+
+$response = new Irpclass($params);
+
+if($response['success'])
+{
+    if(!empty($response['results'])) {
+        //Results
+    }else{
+        //empty
+    }
+}
+```
+
+## Installation
+
+### With Composer
+
+```
+$ composer require tschope/irp-class
+```
+
+```json
+{
+    "require": {
+        "tschope/irp-class": "~0.1"
+    }
+}
+```
+
+```php
+<?php
+require 'vendor/autoload.php';
+
+use Irpclass;
+
+$params = [];
+$params['cat'] = 'Work';
+$params['sbcat'] = 'All';
+$params['typ'] = 'Renewal';
+
+$response = new Irpclass($params);
+
+if($response['success'])
+{
+    if(!empty($response['results'])) {
+        //Results
+    }else{
+        //empty
+    }
+}
+```
+
+<a name="install-nocomposer"/>
+### Without Composer
+
+Why are you not using [composer](http://getcomposer.org/)? Download [Irpclass.php](https://github.com/tschope/irp-class/blob/master/src/Irpclass.php) from the repo and save the file into your project path somewhere. Don't forgot the dependencies, Carbon and Guzzlehttp.
+
+
+
---
+++ src/Irpclass.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Irpclass;
+
+/**
+ * This is the dotenv class.
+ *
+ * It's responsible for loading a `.env` file in the given directory and
+ * setting the environment vars.
+ */
+class Irpclass
+{
+
+    private $url = "https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)";
+
+    public $params = [
+        'cat' => 'Student', // 'Student', 'Work'
+        'sbcat' => 'All',
+        'typ' => 'New',
+    ];
+
+    public $defaultParams = [
+        'verify' => false,
+        'openpage' => false,
+        '_' => false,
+    ];
+
+    /**
+     * Create a new Irpclass instance.
+     *
+     * @param array $params
+     *
+     * @return void
+     */
+    public function __construct(Array $params = null)
+    {
+        if(empty($params))
+        {
+            $this->params = array_merge($this->defaultParams, $this->params);
+        }
+        else
+        {
+            foreach ($params as $key => $value)
+                if(!array_key_exists($key,$this->defaultParams))
+                    return 'Error! You need to set up cat, sbcat and typ';
+
+            $this->params = array_merge($this->defaultParams, $params);
+        }
+    }
+
+    /**
+     * Function to transform retrive date string in carbon date
+     *
+     * @param array $slots
+     *
+     * @return array
+     */
+
+    private function datesTransform(Array $slots)
+    {
+        $return = [];
+        foreach ($slots as $slot)
+        {
+            $time = new \Carbon\Carbon(str_replace(' -','',$slot['time']));
+            $return['time'] = $time;
+            $return['id'] = $slot['id'];
+        }
+        return $return;
+    }
+
+    /**
+     * Function to get data from server
+     *
+     * @return array
+     */
+    public function get()
+    {
+        $client = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false ),'verify' => false));
+        $res = $client->request('GET', $this->url, [
+            'query' => $this->params
+        ]);
+
+        $return = [
+            'success' => false,
+            'error' => null,
+            'message' => null,
+            'results' => []
+        ];
+
+        if($res->getStatusCode() == 200)
+        {
+            $return['success'] = true;
+            $response = \GuzzleHttp\json_decode($res->getBody(),TRUE);
+            if(isset($response['empty'])){
+                if($response['empty']){
+                    $return['message'] = 'Empty. Don\'t have any dates available!';
+                }
+                else
+                {
+                    $return['message'] = $res->getBody();
+                }
+            }
+
+            if(isset($response['slots']))
+            {
+                $return['message'] = 'You have available appoiments';
+                $return['results'] = $this->datesTransform($response['slots']);
+            }
+
+        } else {
+            $return['error'] = $res->getStatusCode();
+            $return['message'] = $res->getBody();
+        }
+
+        return $return;
+
+    }
+
+}
